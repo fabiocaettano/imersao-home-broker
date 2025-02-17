9b454e2dac136aee8a09da356cd9d26fea68e1dc
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-17 23:39:38 +0000

    feat: configuração da entidade assets

--- nestjs-api/src/assets/entities/asset.entity.ts
+++ nestjs-api/src/assets/entities/asset.entity.ts
@@ -1,1 +1,22 @@
-export class Asset {}
+import { Prop, Schema } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class Asset {
+  @Prop()
+  _id: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  symbol: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  price: string;
+
+  createdAt!: Date;
+  updatedAt!: Date;
+}
