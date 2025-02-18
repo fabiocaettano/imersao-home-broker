75cc7bb9c2d2ae37c540fe922910b26d08a80b02
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-18 17:26:01 +0000

    feat: configurado os metodos create e findall

--- nestjs-api/src/orders/orders.controller.ts
+++ nestjs-api/src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 
@@ -12,12 +12,14 @@ export class OrdersController {
   }
 
   @Get()
-  findAll() {
-    return this.ordersService.findAll();
+  findAll(@Query('walletId') walletId: string) {
+    return this.ordersService.findAll({
+      walletId 
+    });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
+    return this.ordersService.findOne(id);
   } 
 }
