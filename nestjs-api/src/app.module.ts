e46d77d90d4ab59d9e1961faa91951e0d6bdef85
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-18 02:48:40 +0000

    feat: teste realizado com sucess incluindo ddados na colecion assetso, mas necessario configurar o arquivo tsconfig.json, habiltando o atributo esMoleInterop.

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AssetsModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:root@24.199.81.137:27017/nest?authSource=admin',
+    ),
+    AssetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
