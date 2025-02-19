235e013675c84c0ae0806b79265878d5181b4d7c
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-19 01:28:52 +0000

    feat: configurado table no arquivo page.tsx para exibir a carteira

--- next-frontend/src/app/page.tsx
+++ next-frontend/src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function AssetsList() {
+import { Table, TableHead, TableHeadCell } from "flowbite-react";
+
+export default function MyAssetsList() {
   return (
-    <div className="">
-    
+    <div className="flex flex-col">
+      <article className="format">
+        <h1>Minha Carteira</h1>
+      </article>
+      <div className="overflox-x-auto w-full">
+        <Table className="w-full max-w-full table-fixed">
+          <TableHead>
+            <TableHeadCell>Ativo</TableHeadCell>
+            <TableHeadCell>Cotação</TableHeadCell>
+            <TableHeadCell>Quantidade</TableHeadCell>
+            <TableHeadCell>Comprar/Vender</TableHeadCell>
+          </TableHead>
+        </Table>
+      </div>    
     </div>
   );
 }
