32a4b416ec198ef5bb5dd569e837e94727817318
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-20 22:37:41 +0000

    feat: configurado o metódo handleMessage; atravé da mensagem 'orders/create' é realizada  a integração cliente servidor

--- nestjs-api/src/orders/orders.gateway.ts
+++ nestjs-api/src/orders/orders.gateway.ts
@@ -1,10 +1,28 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OrdersService } from './orders.service';
+import { OrderType } from './entities/order.entity';
 
 @WebSocketGateway()
 export class OrdersGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): void {
-    client.emit('response', { name: 'ok' });
-    console.log(payload);
+  constructor(private ordersService: OrdersService) {}
+  @SubscribeMessage('orders/create')
+  async handleMessage(
+    client: any,
+    payload: {
+      walletId: string;
+      assetId: string;
+      shares: number;
+      price: number;
+      type: OrderType;
+    },
+  ) {
+    const order = await this.ordersService.create({
+      walletId: payload.walletId,
+      assetId: payload.assetId,
+      shares: payload.shares,
+      price: payload.price,
+      type: payload.type,
+    });
+    return order;
   }
 }
