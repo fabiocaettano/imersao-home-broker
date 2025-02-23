f166a9df485182748abaf5ab8e74a7104037922b
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-23 03:44:34 +0000

    feat: alterado o ip

--- nestjs-api/src/orders/orders.module.ts
+++ nestjs-api/src/orders/orders.module.ts
@@ -43,7 +43,7 @@ import * as kafkaLib from '@confluentinc/kafka-javascript';
       provide: kafkaLib.KafkaJS.Kafka,
       useFactory() {
         return new kafkaLib.KafkaJS.Kafka({
-          'bootstrap.servers': 'localhost:9094',
+          'bootstrap.servers': '192.81.214.94:9094',
         });
       },
     },
