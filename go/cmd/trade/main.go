96b6bf9102fbbcfb7a380169d9ce0dba2c3060de
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-23 23:04:28 +0000

    feat: alterado a poara para 9092

--- go/cmd/trade/main.go
+++ go/cmd/trade/main.go
@@ -27,7 +27,7 @@ func main() {
 	// - group.id: identificador do grupo de consumo
 	// - auto.offset.reset: de onde começar a ler as mensagens
 	consumerConfig := &ckafka.ConfigMap{
-		"bootstrap.servers": "172.19.0.3:9094",
+		"bootstrap.servers": "134.209.113.251:9092",
 		"group.id":          "trade",
 		"auto.offset.reset": "latest",
 	}
@@ -35,7 +35,7 @@ func main() {
 	// Configuração do produtor Kafka
 	// - bootstrap.servers: endereço do broker Kafka
 	producerConfig := &ckafka.ConfigMap{
-		"bootstrap.servers": "172.19.0.3:9094",
+		"bootstrap.servers": "134.209.113.251:9092",
 	}
 
 	// Inicializa produtor e consumidor Kafka
